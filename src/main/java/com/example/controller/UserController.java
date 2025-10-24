d2bfedf95db6660f67d4bf37169fa7f8166060b7
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-10-24 10:15:16 +0100

    add crud users

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,16 +1,51 @@
 package com.example.controller;
 
+import com.example.entity.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
 
-    @GetMapping("/")
-    public String test() {
-        System.out.println("TEST ENDPOINT CALLED");
-        return "API is working!";
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        User existingUser = userService.findById(id);
+        if (existingUser == null) return null;
+
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        return userService.save(existingUser);
+    }
 }
